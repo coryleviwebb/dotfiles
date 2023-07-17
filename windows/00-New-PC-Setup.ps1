f4bb751f4e4a1f477db00bfe34aac2ed76e63e66
Cory Webb <cwebb22@gmail.com>
2023-07-17 09:00:14 -0700

    Add new features

--- windows/00-New-PC-Setup.ps1
+++ windows/00-New-PC-Setup.ps1
@@ -21,7 +21,7 @@ $apps = @(
     @{name = "GPSoftware.DirectoryOpus" },
     @{name = "HandBrake.HandBrake" },
     @{name = "Implbits.HashTab" },
-    # @{name = "LINQPad.LINQPad.6" },
+    @{name = "LINQPad.LINQPad.7" },
     @{name = "Microsoft.AzureStorageExplorer" },
     # MS Office 365
     @{name = "Microsoft.Office" },
@@ -29,6 +29,7 @@ $apps = @(
     @{name = "Microsoft.PowerToys" },
     # @{name = "Microsoft.VisualStudioCode" },
     # @{name = "Microsoft.WindowsTerminal"; source = "msstore" }, 
+    @{name = "Microsoft.SQLServerManagementStudio" },    
     @{name = "Netflix"; source = "msstore" }, 
     @{name = "Notion.Notion" },
     @{name = "Plex.Plex" },
@@ -49,8 +50,9 @@ $apps = @(
 # Install all apps
 foreach ($app in $apps) {
     $listApp = winget list --exact -q $app.name
+    
     if (![String]::Join("", $listApp).Contains($app.name)) {
-        Write-host "Installing: $($app.name)" 
+        Write-Verbose -MessageData "Installing: $($app.name)" 
         if ($null -ne $app.source) {
             winget install --exact --silent "$($app.name)" --source $app.source --accept-package-agreements --accept-source-agreements
         }
@@ -59,7 +61,7 @@ foreach ($app in $apps) {
         }
     }
     else {
-        Write-host "Skipping Install of $($app.name)" 
+        Write-Verbose "Skipping Install of $($app.name) because it is already installed" 
     }
 }
 
@@ -74,7 +76,8 @@ $appsToUninstall = @(
         @{name = "Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe"},
         @{name = "Microsoft.YourPhone_8wekyb3d8bbwe"},
         @{name = "Microsoft.ZuneMusic_8wekyb3d8bbwe"},
-        @{name = "Microsoft.ZuneVideo_8wekyb3d8bbwe"}
+        @{name = "Microsoft.ZuneVideo_8wekyb3d8bbwe"},
+        @{name = "SpotifyAB.SpotifyMusic_zpdnekdrzrea0"}
 );
 
 # Uninstall crap
@@ -86,9 +89,20 @@ foreach ($app in $appsToUninstall) {
         winget uninstall "$($app.name)" --exact --accept-source-agreements
     }
     else{
-        Write-Output "Skipping removal of $($app.name) because it isn't installed"
+        Write-Information "Skipping removal of $($app.name) because it isn't installed"
     }
 }
 
 # Enable Windows Sandbox
+Write-Verbose -Message "Enabling: Windows Sandbox"
 Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart
+
+# Explorer: Show file extensions by default: Hide: 1, Show: 0
+Write-Verbose -Message "Enabling: Show file extensions by default"
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideFileExt" 0
+
+# Opt-In to Microsoft Update
+Write-Verbose -Message "Enabling: Opt-In to Microsoft Update"
+$MU = New-Object -ComObject Microsoft.Update.ServiceManager -Strict
+$MU.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") | Out-Null
+Remove-Variable MU
