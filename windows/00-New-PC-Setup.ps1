29bef80563fc06cb2f583ffc39b15952379965e5
Cory Webb <cwebb22@gmail.com>
2023-07-17 08:59:58 -0700

    Removing features that were added to Powershell and Winget

--- windows/00-New-PC-Setup.ps1
+++ windows/00-New-PC-Setup.ps1
@@ -1,60 +1,8 @@
 #Requires -RunAsAdministrator
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-# Verify we have admin rights
-$user = [Security.Principal.WindowsIdentity]::GetCurrent();
-$isAdmin = (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-
-if(-not $isAdmin){
-    throw "Run this script as administrator so you do not get UAC prompts"
-}
-
-$hasWinget = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-
-if (-not $hasWinget) {
-    throw "Winget not installed.  Download App Installer from the Microsoft Store (https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1#activetab=pivot:overviewtab)"
-}
-
-# Install winget if not installed
-#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
-# $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-# if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-#     "Installing winget Dependencies"
-#     Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-
-#     $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-
-#     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#     $releases = Invoke-RestMethod -uri $releases_url
-#     $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
-
-#     Write-Output -InputObject "Installing winget from $($latestRelease.browser_download_url)"
-#     Write-Output -InputObject "Computer might need to be restarted after installation"
-
-#     Add-AppxPackage -Path $latestRelease.browser_download_url
-# }
-# else {
-#     Write-Output -InputObject
-# }
-
-
-
-# Configure WinGet
-Write-Output "Altering winget settings to allow installation from app store"
-
-# winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
-$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
-$settingsJson = 
-@"
-    {
-        // For documentation on these settings, see: https://aka.ms/winget-settings
-        "experimentalFeatures": {
-          "experimentalMSStore": true,
-        }
-    }
-"@;
-$settingsJson | Out-File $settingsPath -Encoding utf8
+$InformationPreference = "Continue"
+$VerbosePreference = "Continue"
 
 $apps = @(
     @{name = "Adobe.Acrobat.Reader.64-bit" },
